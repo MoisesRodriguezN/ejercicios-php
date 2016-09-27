492430a3a5d46e04ca370d0734ba10c1ebeb99cc
MoisesRodriguezN <moises_rodiguez@hotmail.com>
2016-09-27 11:14:09 +0200

    Ejercicios de piramide corregidos

--- Tema1/ejercicio10.php
+++ Tema1/ejercicio10.php
@@ -5,30 +5,11 @@
   </head>
   <body>
     <?php
-      echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*<br>";
-      echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;**<br>";
-      echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;***<br>";
-      echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;****<br>";
-      echo "&nbsp;&nbsp;&nbsp;&nbsp;*****<br>";
-      echo "&nbsp;&nbsp;&nbsp;******<br>";
-      echo "&nbsp;&nbsp;*******<br>";
-      echo "&nbsp;********<br>";
-      echo "*********";
-
-    ?>
-    
-    <br>
-    
-    <?php
-      echo "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp*<br>";
-      echo "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp**<br>";
-      echo "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp***<br>";
-      echo "&nbsp&nbsp&nbsp&nbsp&nbsp****<br>";
-      echo "&nbsp&nbsp&nbsp&nbsp*****<br>";
-      echo "&nbsp&nbsp&nbsp******<br>";
-      echo "&nbsp&nbsp*******<br>";
-      echo "&nbsp********<br>";
-      echo "*********";
+        echo "&nbsp","&nbsp","&nbsp","&nbsp","&nbsp","&nbsp","&nbsp"," *","<br>";
+        echo "&nbsp","&nbsp","&nbsp","&nbsp","&nbsp"," ***","<br>";
+        echo "&nbsp","&nbsp","&nbsp"," *****","<br>";
+        echo "&nbsp", " *******","<br>";
+        echo "*********","<br>";
     ?>
   </body>
 
--- Tema1/ejercicio11.php
+++ Tema1/ejercicio11.php
@@ -5,16 +5,11 @@
   </head>
   <body>
     <?php
-      echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*&nbsp;&nbsp;&nbsp;&nbsp;<br>";
-      echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*&nbsp;&nbsp;&nbsp;*&nbsp;&nbsp;<br>";
-      echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*<br>";
-      echo "&nbsp;&nbsp;&nbsp;&nbsp;*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*<br>";
-      echo "&nbsp;&nbsp;&nbsp;*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*<br>";
-      echo "&nbsp;&nbsp;*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*<br>";
-      echo "&nbsp;*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*<br>";
-      echo "*&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*<br>";
-      echo "*&nbsp;*&nbsp;*&nbsp;*&nbsp;*&nbsp;*&nbsp;*&nbsp;*&nbsp;*";
-
+        echo "&nbsp","&nbsp","&nbsp","&nbsp","&nbsp","&nbsp","&nbsp"," *","<br>";
+        echo  "&nbsp","&nbsp","&nbsp","&nbsp","&nbsp"," *","&nbsp","&nbsp","*","<br>";
+        echo "&nbsp","&nbsp","&nbsp"," *","&nbsp","&nbsp","&nbsp","&nbsp","&nbsp"," *","<br>";
+        echo "&nbsp", " *","&nbsp","&nbsp","&nbsp","&nbsp","&nbsp","&nbsp","&nbsp","&nbsp","&nbsp","&nbsp","*","<br>";
+        echo "*********","<br>";
     ?>
     
     <br>
