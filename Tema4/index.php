8a849d22c4fc28303032ae41d32da4a74b7e08ac
MoisesRodriguezN <moises_rodiguez@hotmail.com>
2016-09-30 11:30:06 +0200

    Ejercicio Caja fuerte actualizado

--- Tema4/index.php
+++ Tema4/index.php
@@ -7,13 +7,16 @@
   </head>
   <body>
     <?php
-    $digitoUno = rand (0 , 9);
-    $digitoDos = rand (0 , 9);
-    $digitoTres = rand (0 , 9);
-    $digitoCuatro = rand (0 , 9);
+    $numeros;
     $intentos = 4;
     $acertado = false;
     $combinacion = $_GET['combinacion'] . $_GET['digito'];
+    
+    if (isset($_GET['numero'])) {
+      $numeros = $_GET['numero'];
+    }else{
+      $numeros = rand (1000 , 9999);
+    }
     
     if (isset($_GET['intentos'])) {
       $intentos = $_GET['intentos'];
@@ -28,16 +31,16 @@
         $intentos--;
     ?>
     <map name="cajafuerte">
-      <area shape="rect" coords="308,215,330,233" href="index.php?digito=1&&combinacion=<?=$combinacion?>&&intentos=<?=$intentos?>">
-      <area shape="rect" coords="342,212,357,233" href="index.php?digito=2&&combinacion=<?=$combinacion?>&&intentos=<?=$intentos?>">
-      <area shape="rect" coords="376,215,393,233" href="index.php?digito=3&&combinacion=<?=$combinacion?>&&intentos=<?=$intentos?>">
-      <area shape="rect" coords="311,247,327,268" href="index.php?digito=4&&combinacion=<?=$combinacion?>&&intentos=<?=$intentos?>">
-      <area shape="rect" coords="345,247,359,268" href="index.php?digito=5&&combinacion=<?=$combinacion?>&&intentos=<?=$intentos?>">
-      <area shape="rect" coords="377,245,395,268" href="index.php?digito=6&&combinacion=<?=$combinacion?>&&intentos=<?=$intentos?>">
-      <area shape="rect" coords="310,278,326,298" href="index.php?digito=7&&combinacion=<?=$combinacion?>&&intentos=<?=$intentos?>">
-      <area shape="rect" coords="345,278,361,298" href="index.php?digito=8&&combinacion=<?=$combinacion?>&&intentos=<?=$intentos?>">
-      <area shape="rect" coords="372,278,395,292" href="index.php?digito=9&&combinacion=<?=$combinacion?>&&intentos=<?=$intentos?>">
-      <area shape="rect" coords="345,310,360,331" href="index.php?digito=0&&combinacion=<?=$combinacion?>&&intentos=<?=$intentos?>">
+      <area shape="rect" coords="308,215,330,233" href="index.php?digito=1&&combinacion=<?=$combinacion?>&&intentos=<?=$intentos?>&&numero=<?=$numeros?>">
+      <area shape="rect" coords="342,212,357,233" href="index.php?digito=2&&combinacion=<?=$combinacion?>&&intentos=<?=$intentos?>&&numero=<?=$numeros?>">
+      <area shape="rect" coords="376,215,393,233" href="index.php?digito=3&&combinacion=<?=$combinacion?>&&intentos=<?=$intentos?>&&numero=<?=$numeros?>">
+      <area shape="rect" coords="311,247,327,268" href="index.php?digito=4&&combinacion=<?=$combinacion?>&&intentos=<?=$intentos?>&&numero=<?=$numeros?>">
+      <area shape="rect" coords="345,247,359,268" href="index.php?digito=5&&combinacion=<?=$combinacion?>&&intentos=<?=$intentos?>&&numero=<?=$numeros?>">
+      <area shape="rect" coords="377,245,395,268" href="index.php?digito=6&&combinacion=<?=$combinacion?>&&intentos=<?=$intentos?>&&numero=<?=$numeros?>">
+      <area shape="rect" coords="310,278,326,298" href="index.php?digito=7&&combinacion=<?=$combinacion?>&&intentos=<?=$intentos?>&&numero=<?=$numeros?>">
+      <area shape="rect" coords="345,278,361,298" href="index.php?digito=8&&combinacion=<?=$combinacion?>&&intentos=<?=$intentos?>&&numero=<?=$numeros?>">
+      <area shape="rect" coords="372,278,395,292" href="index.php?digito=9&&combinacion=<?=$combinacion?>&&intentos=<?=$intentos?>&&numero=<?=$numeros?>">
+      <area shape="rect" coords="345,310,360,331" href="index.php?digito=0&&combinacion=<?=$combinacion?>&&intentos=<?=$intentos?>&&numero=<?=$numeros?>">
       
     </map>
     
@@ -47,6 +50,12 @@
     <br>
     <?php
       echo $combinacion;
+      echo "la combinacion es", $numeros, "<br>";
+      
+      if($combinacion == $numeros){
+        echo "Se ha abierto la caja fuerte";
+        $acertado = true;
+      }
     ?>
   </body>
 
