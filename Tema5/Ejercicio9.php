822b961c5f47dc18c4156f8e4f29111d370d9f33
MoisesRodriguezN <moises_rodiguez@hotmail.com>
2016-10-19 18:40:43 +0200

    Ejercicio 9 corregido

--- Tema5/Ejercicio9.php
+++ Tema5/Ejercicio9.php
@@ -31,8 +31,6 @@
         //Primera visita
         $cuentaNumeros = 0;
         $numeros = "";
-        $posInicial = 0;
-        $posFinal = 0;
       }
       
       if(isset($n) && $cuentaNumeros <=10){
@@ -44,15 +42,18 @@
       if ($cuentaNumeros == 10){
         $numeros = substr($numeros, 1);
         $arrayNumeros = explode(" ", $numeros);
-
+         
+        echo "<br>";
+        echo "Array original";
+        echo "<br>";
         for($i = 0; $i < count($arrayNumeros); $i++){
-          echo"   " , $i , "   ";
+        echo "&nbsp;", $i,"&nbsp;";
         }
 
         echo "<br>";
-
+        
         for($i = 0; $i < count($arrayNumeros); $i++){
-          echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;", $arrayNumeros[$i];
+          echo "&nbsp;", $arrayNumeros[$i], "&nbsp;";
         }
 
         echo "<br>";
@@ -60,42 +61,56 @@
         $numeroInicial = $arrayNumeros[$posInicial];
         $numeroFinal = $arrayNumeros[count($arrayNumeros)-1];
 
-        for($i = count($arrayNumeros)-1; $i >= $posFinal; $i--){
+        for($i = count($arrayNumeros)-1; $i >= $posFinal; $i--){ 
           $arrayNumeros[$i] = $arrayNumeros[$i - 1];
         }
+        
+         
 
         $arrayNumeros[$posFinal] = $numeroInicial;
+        
+       
 
         for($i = $posInicial; $i > 0; $i--){
           $arrayNumeros[$i] = $arrayNumeros[$i - 1];
-        }
-
+       }
+    
+       
+        echo "<br>";
+        echo "Array modificado";
+        echo "<br>";
+       
+        
         $arrayNumeros[0] = $numeroFinal;
-
-        //Muestra de nuevo el array
-
-        for($i = 0; $i < $arrayNumeros; $i++){
-          echo "   " ,  $i , "   ";
+        
+        for($i = 0; $i < count($arrayNumeros); $i++){
+          echo "&nbsp;", $i, "&nbsp;";
         }
 
         echo ("<br>");
 
         for($i = 0; $i < count($arrayNumeros); $i++){
-          echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;", $arrayNumeros[$i];
+          echo "&nbsp;", $arrayNumeros[$i], "&nbsp;";
         }
       }else{
         ?>  
-        <form action="Ejercicio8.php" method="get">
+        <form action="Ejercicio9.php" method="get">
         Número <input type="number" autofocus name="n"><br>
-        Posicion incial <input type="number" autofocus name="posInicial"><br>
-        Posicion final <input type="number" autofocus name="posFinal"><br>
         <input type="hidden" name="cuentaNumeros" value="<?php echo $cuentaNumeros; ?>">
-         <input type="hidden" name="numeros" value="<?php echo $numeros; ?>">
-         <input type="hidden" name="numeros" value="<?php echo $posInicial; ?>">
-         <input type="hidden" name="numeros" value="<?php echo $posFinal; ?>">
-         <input type="submit" value="Aceptar">  
+        <input type="hidden" name="numeros" value="<?php echo $numeros; ?>">
+       
+        <?php
+        
+          if($cuentaNumeros == 9){
+            ?>
+            Posicion incial <input type="number" name="posInicial"><br>
+            Posicion final <input type="number" name="posFinal"><br>
+        <?php
+          }
+        ?>
+        <input type="submit" value="Aceptar">  
         </form>
-        <?php  
+        <?php   
       }
   
     ?>
