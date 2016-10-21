92a4514d8a7ea084ddfa1c6d6901875c42043383
MoisesRodriguezN <moises_rodiguez@hotmail.com>
2016-10-21 10:58:53 +0200

    Ejercicio diccionario actualizado

--- Tema5/index.php
+++ Tema5/index.php
@@ -2,10 +2,7 @@
 <html>
   <head>
     <meta charset="UTF-8">
-        <title>Diccionario Español-Ingles</title>
-        <style>
-        </style>
-        
+        <title>Diccionario Español-Ingles</title>  
   </head>
   <body>
     <?php
@@ -19,31 +16,43 @@
             $palabraEspanol[]=$key;
         }
         
-        $palabraIntroducida = $_GET["palabraIntroducida"];
+        $ingles = $_GET["ingles"];
         
-        if(!isset($palabraIntroducida)){
+        if(!isset($ingles)){
         ?>
       
     <?php
-        $palabrasGeneradas = [];//Inicializo el array de palabrasGeneradas
+        $espanol = [];//Inicializo el array de palabrasGeneradas
         for ($i = 0; $i < 5; $i++){
             do{
             $palabra=$palabraEspanol[rand(0, 19)];//guarda palabra una palabra aleatoria
-            }while(in_array($palabra, $palabrasGeneradas));//comprueba que este guardada en el array
-            $palabrasGeneradas[]=$palabra;//si no esta dentro del array la guarda
+            }while(in_array($palabra, $espanol));//comprueba que este guardada en el array
+            $espanol[]=$palabra;//si no esta dentro del array la guarda
         }   
           
-      echo '<form action="pagina.php" method="get">';
+      echo '<form action="index.php" method="get">';
       for ($i = 0; $i < 5; $i++) {
         echo $espanol[$i]." ";
         echo '<input type="hidden" name="espanol['.$i.']" value="'.$espanol[$i].'">';
         echo '<input type="text" name="ingles['.$i.']" ><br>';
       }
+      
+      echo '<input type="submit" value="Aceptar">';
+      echo '</form>';
         }else{
-            
+          $espanol = $_GET['espanol'];
+          $ingles = $_GET['ingles'];
+          for ($i = 0; $i < 5; $i++) {
+            if ($palabras[$espanol[$i]] == $ingles[$i]) {
+              echo '<span style="color: green;">'.$espanol[$i].": ".$ingles[$i];
+              echo " - correcto</span><br>";
+            } else {
+              echo '<span style="color: red;">'.$espanol[$i].": ".$ingles[$i];
+              echo " - incorrecto</span>, la respuesta correcta es <b>".$palabras[$espanol[$i]]."</b><br>";
+            }
+          }
         }
     
     ?>
   </body>
-
 </html>
